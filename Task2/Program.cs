bbfc201376c8ee111ee494e394805434829b38a3
Dmitriy <dmitriykharitonov333@gmail.com>
2023-12-17 21:47:13 +0300

    Решил 2 задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+// Задайте массив заполненный случайными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); 
+
+}
+
+bool CheckCondition(int x)
+{
+    if (x%2 == 0) 
+            return true;
+        return false;
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (CheckCondition(array[i]) == true)
+            count ++;
+    Console.WriteLine($"Количество чётных чисел в массиве: {count}");
+}
+
+int[] array = new int[10]; 
+InputArray(array);
+
+Console.WriteLine(string.Join(", ", array));
+ReleaseArray(array);
